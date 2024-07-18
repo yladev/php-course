fae8a45a86d4271d22eddb90d84baf4c75352f20
Yan Linn Aung <yanlinnaung@192.168.1.4>
2024-07-18 17:51:11 +0630

    update for loop

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Testing</title>
+</head>
+<body>
+    <h2>Testing</h2>
+
+    <body>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Aspernatur, labore!
+    </body><br>
+
+    <a href="test.php">Testing</a><br>
+
+    <hr>
+
+    <?php
+    echo "Hello World!";
+    ?>
+    
+    <?php
+    // iteration - looping
+    for ($i=1; $i <= 10; $i++) { // for loop
+        echo "<h1>Hello $i</h1>"; // php statement 
+    }
+    ?>
+
+    <?php
+    for ($i=1900; $i <= 2024 ; $i++) { 
+        echo "$i";
+    }
+    ?>
+</body>
+</html>
