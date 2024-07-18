5e9f1038467d1575ca8a8f5adad7873a49231e7e
Yan Linn Aung <yanlinnaung@192.168.1.4>
2024-07-18 18:45:18 +0630

    update only php testing code

---
+++ testing.php
@@ -0,0 +1,17 @@
+<?php
+echo "Hello<br>";
+echo "How are you?<br>";
+echo "I'm Yan Linn Aung<br>";
+?>
+
+<?php
+for ($i=0; $i <= 10 ; $i++) { 
+    echo "Hi<br>";
+}
+?>
+
+<?php
+    $name = "Yan Linn Aung";
+    echo "$name<br>";
+    echo "Hello $name";
+?>
