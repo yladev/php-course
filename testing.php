390b42a5fda9947590def240ea1bc54cf2595196
Yan Linn Aung <yanlinnaung@Yans-MacBook-Air.local>
2024-07-20 20:35:07 +0630

    update looping

--- testing.php
+++ testing.php
@@ -5,7 +5,7 @@ echo "I'm Yan Linn Aung<br>";
 ?>
 
 <?php
-for ($i=0; $i <= 10 ; $i++) { 
+for ($i=0; $i<=10; $i++) { 
     echo "Hi<br>";
 }
 ?>
@@ -13,5 +13,29 @@ for ($i=0; $i <= 10 ; $i++) {
 <?php
     $name = "Yan Linn Aung";
     echo "$name<br>";
-    echo "Hello $name";
+    echo "Hello $name<br>";
+?>
+
+<?php
+echo "<hr>";
+
+for ($i=1; $i<=10; $i++) { //$i=$i-2
+    echo "Hello $i<br>";
+}
+
+echo  "<hr>";
+
+$j=1;
+while ($j<=10) {
+    echo "Hello $j<br>";
+    $j++; //increment
+}
+
+echo "<hr>";
+
+$k=1;
+do {
+    echo "Hello $k<br>";
+    $k++; //increment
+} while ($k <= 10); // end condition, at least once
 ?>
